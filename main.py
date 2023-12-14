bd08b4bc78c5dcadb390aacdfe926b2eecefd471
anjor <anjor@umd.edu>
2023-12-14 13:13:57 +0000

    license

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
 import os
+import argparse
 from openai import OpenAI
 from pymongo import MongoClient
 
-from ingest import ingest, create_search_index, add_embedding
+from ingest import ingest
 from search import query_results
 
 MONGO_URI = os.environ.get("MONGO_URI")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ingest", action="store_true", help="Run ingest pipeline")
+parser.add_argument("--query", action="store_true", help="Run query")
+args = parser.parse_args()
 
 if __name__ == "__main__":
     # Set up mongo and openai clients
     mongo_client = MongoClient(MONGO_URI)
     openai_client = OpenAI(
-        base_url="https://demo.deployradiant.com/mongodb-rag-example/openai/",
+        base_url="https://demo.deployradiant.com/mongodb-rag-example/openai",
         api_key="notNeeded",
     )
 
-    # Ingest the pdf
     pdf_path = "0704.0044.pdf"
     db = mongo_client.plasmaPhysics
     collection = db.pdfs
 
     embedding_field_name = "openai_text_embedding_ada_002"
-    # ingest(openai_client=openai_client, pdf_file=pdf_path, collection=collection, embedding_field_name=embedding_field_name)
-    # create_search_index(collection=collection, embedding_field_name=embedding_field_name, index=embedding_field_name + "_index", dimensions=1536)
 
-    mistral_openai_client = OpenAI(
-        base_url="https://demo.deployradiant.com/mistral-embedding/openai/",
-        api_key="notNeeded",
-    )
-    # add_embedding(openai_client=mistral_openai_client, collection=collection, embedding_field_name="mistral_embedding")
-    # Query the pdf
-    query = "What is Goldreich-Sridhar scaling?"
-    result = query_results(
-        openai_client=openai_client,
-        collection=collection,
-        field=embedding_field_name,
-        index=embedding_field_name + "_index",
-        query=query,
-    )
-    print(result)
+    if args.ingest:
+        # Ingest the pdf
+        ingest(openai_client=openai_client, pdf_file=pdf_path, collection=collection, embedding_field_name=embedding_field_name)
+        # The following line has been commented out because it needs a newer version of MongoDB Cluster.
+        # create_search_index(collection=collection, embedding_field_name=embedding_field_name, index=embedding_field_name + "_index", dimensions=1536)
 
-    query = "What is Goldreich-Sridhar scaling?"
-    result = query_results(
-        openai_client=mistral_openai_client,
-        collection=collection,
-        field="mistral_embedding",
-        index="vector_index",
-        query=query,
-    )
-    print(result)
+    if args.query:
+        # Query the pdf
+        query = "What is Goldreich-Sridhar scaling?"
+        result = query_results(
+            openai_client=openai_client,
+            collection=collection,
+            field=embedding_field_name,
+            index=embedding_field_name + "_index",
+            query=query,
+        )
+        print(result)
+
